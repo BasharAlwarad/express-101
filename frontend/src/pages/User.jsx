76fe298ef1af2b185cce61a654e14d099f85b00c
Bashar Alwarad <beelwarad52@gmail.com>
2024-06-11 16:30:17 +0200

    frontend users

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -1,7 +1,35 @@
+import {useState,useEffect} from 'react'
+import {useParams} from 'react-router-dom'
+import axios from 'axios'
 
 const User = () => {
+  const {id}=useParams()
+  const [user, setUser] = useState(null)
+
+  const getSingleUser =async (id) => { 
+try {
+  const {data}=await axios.get(`http://localhost:8000/api/v1/users/${id}`)
+  setUser(data[0])
+} catch (error) {
+  console.log(error.message)
+}
+   }
+
+   useEffect(() => {
+     getSingleUser(id)
+   }, [id])
+   
+  
   return (
-    <div>User</div>
+    <div>
+      {
+        user && (
+          <div>
+            <h1>{user?.name}</h1>
+          </div>
+        )
+      }
+    </div>
   )
 }
 
--- frontend/src/pages/Users.jsx
+++ frontend/src/pages/Users.jsx
@@ -1,7 +1,37 @@
+import {useState,useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import axios from 'axios'
+
 
 const Users = () => {
+  const [users, setUsers] = useState([])
+
+  const getAllUsers =async () => { 
+    try {
+      const {data}=await axios.get(`http://localhost:8000/api/v1/users`)
+      setUsers(data)
+      
+    } catch (error) {
+      console.log(error.message)
+    }
+   }
+
+   useEffect(() => {
+     getAllUsers()
+   
+   }, [])
+   
+
   return (
-    <div>Users</div>
+    <div>
+{
+  users?.map((user)=>(
+    <div key={user.id}>
+      <Link to={`/users/${user.id}`}>{user.name}</Link>
+    </div>
+  ))
+}
+    </div>
   )
 }
 
